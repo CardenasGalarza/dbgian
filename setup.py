a30d2824df384db0e29f1118d48193c4215704e9
CardenasGalarza <alex10estadistica@gmail.com>
2024-01-04 21:26:52 -0500

    DSD

--- setup.py
+++ setup.py
@@ -9,12 +9,8 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'dbgian = dbgian:main',  # Asegúrate de que el nombre del módulo sea correcto
+            'dbgian = mimodulo:main',  # Reemplaza 'mimodulo' con el nombre real de tu módulo y 'main' con la función principal si la tienes
         ],
     },
-    description='Una breve descripción de tu paquete.',
-    long_description='Descripción más detallada, instrucciones de uso, etc.',
-    author='Giancarlos Cardenas Galarza',
-    author_email='alex10estadistica@gmail.com',
-    url='https://github.com/CardenasGalarza/dbgian.git',
 )
+
