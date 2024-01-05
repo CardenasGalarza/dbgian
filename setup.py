0345eb78143e417c1882a06a59c9e6e0eccbb944
CardenasGalarza <alex10estadistica@gmail.com>
2024-01-04 20:07:14 -0500

    SSS

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dbgian',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Aquí puedes agregar las dependencias si las tienes
+    ],
+)
