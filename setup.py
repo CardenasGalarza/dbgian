29b9635773c24d52788f6c147e23562da5dfeb2a
CardenasGalarza <alex10estadistica@gmail.com>
2024-01-04 21:28:36 -0500

    SDSDD

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dbgian',
-    version='0.1.0',
+    version='0.2.0',  # Cambia la versión a la nueva
     packages=find_packages(),
     install_requires=[
         # Aquí puedes agregar las dependencias si las tienes
@@ -13,4 +13,3 @@ setup(
         ],
     },
 )
-
